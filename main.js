b684df0638cbad2482ac15ef80d7830dcc4b563a
al <107424647+aladin20c@users.noreply.github.com>
2024-03-09 16:46:33 +0100

    Update main.js
    -reload page after first load

--- main.js
+++ main.js
@@ -487,7 +487,12 @@ function worldUpdate(ctx){
 }
 
 
-
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
 
 
 /************************************************/
